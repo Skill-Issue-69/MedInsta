71963141cfc1da09cca6a714c45ab13ac408fe8d
ankyson23 <aniket.sonawane1734@gmail.com>
2025-03-20 23:42:42 +0530

    initial setup

---
+++ backend/chatbot/views.py
@@ -0,0 +1,23 @@
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
+from .models import Chat
+from .serializers import ChatSerializer
+from .ml.predict import chatbot_response  # ML integration
+
+
+@api_view(["POST"])
+def chat(request):
+    user_input = request.data.get("message", "")
+    bot_reply = chatbot_response(user_input)  # ML model response
+
+    chat = Chat.objects.create(user_input=user_input, bot_response=bot_reply)
+    serializer = ChatSerializer(chat)
+
+    return Response(serializer.data)
+
+
+@api_view(["GET"])
+def chat_history(request):
+    chats = Chat.objects.all()
+    serializer = ChatSerializer(chats, many=True)
+    return Response(serializer.data)
